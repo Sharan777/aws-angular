77807ac3526f427bdd635c92423ad1fdc8c30f09
Sharan777 <sharangouda021@gmail.com>
2020-02-19 15:58:24 +0530

    1 file modified, 17 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CompareComponent } from './compare/compare.component';
+// import { UserComponent } from './user/user.component';
+import { CompareInputComponent } from './compare/compare-input/compare-input.component';
+import { CompareResultComponent } from './compare/compare-result/compare-result.component';
+import { CompareService } from './compare/compare.service';
+import { SigninComponent } from './user/signin/signin.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { AuthService } from './user/auth.service';
+import { AuthGuardService } from './user/auth-guard.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, CompareComponent,  CompareInputComponent, CompareResultComponent, SigninComponent, SignupComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [CompareService, AuthService, AuthGuardService]
 })
 export class AppModule { }
---
+++ src/app/compare/compare-input/compare-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-input',
+  templateUrl: './compare-input.component.html',
+  styleUrls: ['./compare-input.component.css']
+})
+export class CompareInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
